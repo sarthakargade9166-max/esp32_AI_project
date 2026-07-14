1aad2343b8825cc159f12510e1c827ababa4166e
sarthakargade9166-max <sarthakargade9166@gmail.com>
2026-07-14 12:22:35 +0530

    used forestregressor

--- ml_code.py
+++ ml_code.py
@@ -1,36 +1,33 @@
 import os
 import numpy as np
 import pandas as pd 
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 df= pd.read_csv('queue_data.csv')
 X = df[[
     "queue_count",
     "avg_service_time",
-    "active_counters",
     "hour_of_day",
     "day_of_week"
 ]]
 y= df["actual_wait"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = LinearRegression()  
+model = RandomForestRegressor(n_estimators=100,random_state=42) 
 model.fit(X_train, y_train)
 
 
 new_data = pd.DataFrame({
-    "queue_count": [100],
+    "queue_count": [1],
     "avg_service_time": [5.0],
-    "active_counters": [1],
     "hour_of_day": [11],
     "day_of_week": [0]
 })
 
-##print("Actual wait:", model.predict(new_data))
-#print(new_data)
 
 prediction = model.predict(new_data)
+print(f"Predicted waiting time: {prediction[0]} minutes")
  
 if prediction[0] < 5:
     from twilio_msg import send_whatsapp_message
